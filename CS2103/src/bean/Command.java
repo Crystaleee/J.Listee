4a8acd5adc63934654353201f0a242d85f45d388
herk-lin <a0126070@u.nus.edu>
2016-03-03 12:14:25 +0800

    Added more methods and attributes

--- CS2103/src/bean/Command.java
+++ CS2103/src/bean/Command.java
@@ -20,15 +20,32 @@ public class Command {
 	private  String deleteLocation;
 	private  ArrayList<String> tagLists = new ArrayList<String>();
 	private ArrayList<Integer> taskNumber;
+	private ArrayList<Integer> deleteTaskNumber = new ArrayList<Integer>();
+	private Integer numberOfDaysToPostpone;
+	private ArrayList<Integer> doneTaskNumber = new ArrayList<Integer>();
 	
 	public String getCommandType(){
 		return commandType;
 	}
 	
-	public void setTaskNumber(int[] taskNum) {
-		for(int num:taskNum){
-			taskNumber.add(num);		
-		}	
+	public void setDoneTaskNumber(Integer taskNum) {
+		doneTaskNumber.add(taskNum);
+	}
+	
+	public ArrayList<Integer> getDoneTaskNumber(){
+		return doneTaskNumber;
+	}
+
+	public void setNumberOfDaysToPostpone(Integer days) {
+		numberOfDaysToPostpone = days;
+	}
+	
+	public int getNumberOfDaysToPostpone() {
+		return numberOfDaysToPostpone;
+	}
+
+	public void setTaskNumber(Integer taskNum) {
+		taskNumber = taskNum;		
 	}
 
 	public void setTags(String tag) {
@@ -47,14 +64,14 @@ public class Command {
 		return date;
 	}
 	
-//	public void setEndDateTime(int endDateYear, int endDateMonth, int endDateDay, int endHour, int endMin) {
-//		endDateTime.set(endDateYear,endDateMonth,endDateDay,endHour,endMin);
-//		
-//	}
-//
-//	public void setStartDateTime(int startDateYear, int startDateMonth, int startDateDay, int startHour, int startMin) {
-//		startDateTime.set(startDateYear,startDateMonth,startDateDay,startHour,startMin);
-//	}
+	public void setEndDateTime(int endDateYear, int endDateMonth, int endDateDay, int endHour, int endMin) {
+		endDateTime.set(endDateYear,endDateMonth,endDateDay,endHour,endMin);
+		
+	}
+
+	public void setStartDateTime(int startDateYear, int startDateMonth, int startDateDay, int startHour, int startMin) {
+		startDateTime.set(startDateYear,startDateMonth,startDateDay,startHour,startMin);
+	}
 	
 	
 	public void setCommandType(String commandType) {
@@ -63,87 +80,42 @@ public class Command {
 	}
 	
 	public void setTaskDescription(String input){
-		this.taskDescription=input;
+		taskDescription.add(input);
 	}
-	public String getTaskDescription(){
+	public ArrayList<String> getTaskDescription(){
 		return taskDescription;
 	}
 	
-//	public Calendar getStartDateTime(){
-//		return startDateTime;
-//	}
-//
-//	public Calendar getEndDateTime(){
-//		return endDateTime;
-//	}
-//	
-//	public String getLocation(){
-//		return location;
-//	}
+	public Calendar getStartDateTime(){
+		return startDateTime;
+	}
+
+	public Calendar getEndDateTime(){
+		return endDateTime;
+	}
+	
+	public String getLocation(){
+		return location;
+	}
 	
 	public ArrayList<String> getTags(){
 		return tagLists;
 	}
 	
-//	public void setLocation(String location){
-//		this.location = location;	
-//	}
+	public void setLocation(String location){
+		this.location = location;
+		
+	}
 
-	public  ArrayList<Integer> getTaskNumber() {
+	public  int getTaskNumber() {
 		return taskNumber;
 	}
 
-	public void setTaskNumber(Integer valueOf) {
-		this.taskNumber.set(0, valueOf);
-	}
-
-	public Calendar getAddStartDate() {
-		return addStartDate;
-	}
-
-	public Calendar getAddEndDate() {
-		return addEndDate;
-	}
-
-	public Calendar getDeleteStartDate() {
-		return deleteStartDate;
-	}
-
-	public void setDeleteStartDate(Calendar deleteStartDate) {
-		this.deleteStartDate = deleteStartDate;
-	}
-
-	public Calendar getDeleteEndDate() {
-		return deleteEndDate;
-	}
-
-	public void setDeleteEndDate(Calendar deleteEndDate) {
-		this.deleteEndDate = deleteEndDate;
-	}
-
-	public String getAddLocation() {
-		return addLocation;
-	}
-
-	public void setAddLocation(String addLocation) {
-		this.addLocation = addLocation;
-	}
-
-	public String getDeleteLocation() {
-		return deleteLocation;
-	}
-
-	public void setDeleteLocation(String deleteLocation) {
-		this.deleteLocation = deleteLocation;
-	}
-
-	public void setAddStartDateTime(int startDateYear, int startDateMonth,
-			int startDateDay, int startHour, int startMin) {
-		this.addStartDate.set(startDateYear, startDateMonth, startDateDay, startHour, startMin);
+	public ArrayList<Integer> getDeleteTaskNumber(){
+		return deleteTaskNumber;
 	}
 	
-	public void setAddEndDateTime(int endDateYear, int endDateMonth,
-			int endDateDay, int endHour, int endMin) {
-		this.addEndDate.set(endDateYear, endDateMonth, endDateDay, endHour, endMin);
+	public void setDeleteTaskNumber(Integer taskNum){
+		deleteTaskNumber.add(taskNum);
 	}
 }
