d882a322001d542173a3d828416b039e1445ed1c
bthjehiel <jehielmisc@gmail.com>
2016-03-06 02:05:22 +0800

    Implement redo command

---
+++ CS2103/src/bean/CommandRedo.java
@@ -0,0 +1,43 @@
+/*
+ * Written by Boh Tuang Hwee, Jehiel (A0139995E)
+ * Last updated: 3/6/2016, 1:38am
+ * CS2103
+ */
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CommandRedo extends Command{
+    private final String MESSAGE_REDO = "Redid last command";
+    private final String MESSAGE_ERROR_REDO = "You have reached the latest point possible";
+    private final String MESSAGE_ERROR = "Error occured while updating to file";
+    
+    public CommandRedo(){
+    }
+    
+    public Display execute(Display display){
+        if(History.atLastState()){
+            display = new Display(MESSAGE_ERROR_REDO);
+            return display;
+        }
+        
+        display = History.getDisplay(1);
+        if(updateFile(display)){
+            display.setMessage(MESSAGE_REDO);
+            History.incrementIndex();
+        }
+        else{
+            display = new Display(MESSAGE_ERROR);
+        }
+        return display;
+    }
+    
+    public boolean updateFile(Display display) {
+        try{
+            Storage.saveFile(display);
+            return true;
+        }catch(IOException error){
+            return false;
+        }
+    }
+}
