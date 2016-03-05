21966ca2985a6c328a6446c3b0a677add56c5469
bthjehiel <jehielmisc@gmail.com>
2016-03-06 01:59:45 +0800

    Implement invalid command class

---
+++ CS2103/src/bean/CommandInvalid.java
@@ -0,0 +1,16 @@
+/*
+ * Written by Boh Tuang Hwee, Jehiel (A0139995E)
+ * Last updated: 3/5/2016, 8:00pm
+ * CS2103
+ */
+public class CommandInvalid extends Command{
+    private final String MESSAGE_INVALID_COMMAND = "Pls enter a valid command";
+    
+    public CommandInvalid(){
+    }
+    
+    public Display execute(Display display){
+        display.setMessage(MESSAGE_INVALID_COMMAND);;
+        return display;
+    }
+}
