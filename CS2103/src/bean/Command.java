733dde87a157ffcb852a85e3df18e15aae2aadcc
bthjehiel <bthje_000@Stud>
2016-03-16 15:29:03 +0800

    updated Command interface

--- CS2103/src/bean/Command.java
+++ CS2103/src/bean/Command.java
@@ -11,28 +11,3 @@ public interface Command {
 
     public boolean getUpdateFile();
 }
-/*
- * class ShowCommand extends Command{ private final String
- * MESSAGE_NO_TASKS_FOUND = "No such tasks found"; private String keyword;
- * 
- * public ShowCommand(){ this.keyword = null; }
- * 
- * public ShowCommand(String keyword){ this.keyword = keyword; }
- * 
- * public Display execute(ArrayList<Task> taskList){ if(keyword == null){
- * setDisplay(null, History.getTaskList(0)); return getDisplay(); }
- * 
- * ArrayList<Task> searchedTasks = null; searchedTasks =
- * getTasksContainingKeyword(taskList); if(searchedTasks.isEmpty()){
- * setDisplay(MESSAGE_NO_TASKS_FOUND, null); } else{ setDisplay(null,
- * searchedTasks); }
- * 
- * return getDisplay(); }
- * 
- * public ArrayList<Task> getTasksContainingKeyword(ArrayList<Task> taskList) {
- * ArrayList<Task> tasksContainingKeyword = new ArrayList<Task>(); for(int i =
- * 0; i < taskList.size(); i++){
- * if(taskList.get(i).getDescription().contains(keyword)){
- * tasksContainingKeyword.add(taskList.get(i)); } } return
- * tasksContainingKeyword; } }
- */
