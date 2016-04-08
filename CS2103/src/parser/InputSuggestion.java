8b2fd3922e07a84667f8eaf58eb0fe803a7dd3af
fortunac <cfortuna@bu.edu>
2016-04-08 23:07:21 +0800

    Update suggestions to fit new window size

--- CS2103/src/parser/InputSuggestion.java
+++ CS2103/src/parser/InputSuggestion.java
@@ -8,60 +8,93 @@ import logic.Logic;
 
 public class InputSuggestion {
 
-	private static final String SUGGESTION_ADD = "add  <description>  at/from/due/etc  [time(s)]  [@location]  [#tag]";
-	private static final String SUGGESTION_DELETE = "delete  <task number(s)>";
+	private static final String SUGGESTION_ADD = "add <description> [\"at/from/due/etc\"] [start time] [\"to\" end time] [@location] [#tag]";
 
-	private static final String SUGGESTION_UPDATE = "update  <task number>  [new description]  [/time-group time]  [(-)@location]  [(-)#tag]";
-	private static final String SUGGESTION_UPDATE_FLOAT = "update  <task number>  [new description]  [(-)@location]  [(-)#tag]";
-	private static final String SUGGESTION_UPDATE_DEADLINE = "update  <task number>  [new description]  [/end (-)[time]]  [(-)@location]  [(-)#tag]";
-	private static final String SUGGESTION_UPDATE_EVENT = "update  <task number>  [new description]  [/time-group (-)time(s)]  [(-)@location]  [(-)#tag]";
-	private static final String SUGGESTION_UPDATE_RESERVED = "update  <task #>  [reservation index]  [new description]  [/time-group time(s)]  [(-)@location]  [(-)#tag]";
+	private static final String SUGGESTION_DEL = "del <task number(s)>";
+	private static final String SUGGESTION_DELETE = "delete <task number(s)>";
 
-	private static final String SUGGESTION_EDIT = "edit  <task number>  [new description]  [/time-group time]  [(-)@location]  [(-)#tag]";
-	private static final String SUGGESTION_EDIT_FLOAT = "edit  <task number>  [new description]  [(-)@location]  [(-)#tag]";
-	private static final String SUGGESTION_EDIT_DEADLINE = "edit  <task number>  [new description]  [/end (-)[time]]  [(-)@location]  [(-)#tag]";
-	private static final String SUGGESTION_EDIT_EVENT = "edit  <task number>  [new description]  [/time-group (-)time(s)]  [(-)@location]  [(-)#tag]";
-	private static final String SUGGESTION_EDIT_RESERVED = "edit  <task #>  [reservation index]  [new description]  [/time-group time(s)]  [(-)@location]  [(-)#tag]";
-	
-	private static final String SUGGESTION_RESERVE = "reserve  <description>  <start date(s) to end date(s)>  [@location]  [#tag]";
-	private static final String SUGGESTION_CONFIRM = "confirm  <task number(s)>  <reservation index>";
+	private static final String SUGGESTION_UPDATE = "update <task number> [description] [(-)/time-group time(s)] [(-)@location] [(-)#tag]";
+	private static final String SUGGESTION_UPDATE_FLOAT = "update <task number> [new description] [(-)@location] [(-)#tag]";
+	private static final String SUGGESTION_UPDATE_DEADLINE = "update <task number> [new description] [(-)/end [time]] [(-)@location] [(-)#tag]";
+	private static final String SUGGESTION_UPDATE_EVENT = "update <task number> [description] [(-)/time-group time(s)] [(-)@location] [(-)#tag]";
+	private static final String SUGGESTION_UPDATE_RESERVED = "<task #> [reserved block #] [description] [/time-group time(s)] [(-)@location] [(-)#tag]";
+
+	private static final String SUGGESTION_EDIT = "edit <task number> [description] [(-)/time-group time(s)] [(-)@location] [(-)#tag]";
+	private static final String SUGGESTION_EDIT_FLOAT = "edit <task number> [new description] [(-)@location] [(-)#tag]";
+	private static final String SUGGESTION_EDIT_DEADLINE = "edit <task number> [new description] [(-)/end [time]] [(-)@location] [(-)#tag]";
+	private static final String SUGGESTION_EDIT_EVENT = "edit <task number> [description] [(-)/time-group time(s)] [(-)@location] [(-)#tag]";
+	private static final String SUGGESTION_EDIT_RESERVED = "<task #> [reserved block #] [description] [/time-group time(s)] [(-)@location] [(-)#tag]";
+
+	private static final String SUGGESTION_RES = "res <description> <\"from\" start date(s) \"to\" end date(s)> [@location] [#tag]";
+	private static final String SUGGESTION_RESERVE = "reserve <description> <\"from\" start date(s) \"to\" end date(s)> [@location] [#tag]";
+
+	private static final String SUGGESTION_CFM = "cfm <task number> <reserved block number>";
+	private static final String SUGGESTION_CONFIRM = "confirm <task number> <reserved block number>";
+
 	private static final String SUGGESTION_DONE = "done  <task number(s)>";
 	private static final String SUGGESTION_UNDONE = "undone  <task number(s)>";
-	private static final String SUGGESTION_SHOW = "show  [/task-group]  [keyword]  [location]  [tag]";
-	private static final String SUGGESTION_POSTPONE = "postpone  <task number(s)>  <time to postpone by>";
+
+	private static final String SUGGESTION_SEARCH = "search [/task-group] [keyword] [@location] [#tag]";
+	private static final String SUGGESTION_SHOW = "show [/task-group] [keyword] [@location] [#tag]";
+
+	private static final String SUGGESTION_POSTPONE = "postpone <task number> <time to postpone by>";
+	private static final String SUGGESTION_PP = "pp <task number> <time to postpone by>";
+
 	private static final String SUGGESTION_UNDO = "undo";
 	private static final String SUGGESTION_REDO = "redo";
+
 	private static final String SUGGESTION_FILEPATH = "change filepath";
+
 	private static final String SUGGESTION_HELP = "help";
 	private static final String SUGGESTION_EXIT = "exit";
+
 	private static final String SUGGESTION_INVALID_COMMAND = "Invalid command!";
 	private static final String SUGGESTION_INVALID_TASK = "Invalid task number!";
 
 	private static final String SUGGESTION_EDIT_EXIT = "edit <task number> // exit";
-	private static final String SUGGESTION_CONFIRM_FILEPATH = "confirm <task number(s)> // change filepath";
-	private static final String SUGGESTION_DELETE_DONE = "delete <task number(s)> // done <task number(s)>";
+	private static final String SUGGESTION_CONFIRM_FILEPATH = "cfm/confirm <task number(s)> <reserved block number(s)> // change filepath";
+	private static final String SUGGESTION_DELETE_DEL = "del/delete <task number(s)>";
+	private static final String SUGGESTION_DELETE_DONE = "del/delete <task number(s)> // done <task number(s)>";
+	private static final String SUGGESTION_POSTPONE_PP = "pp/postpone <task number(s)> <time to postpone by>";
+	private static final String SUGGESTION_SHOW_SEARCH = "show/search [/task-group] [keyword] [@location] [#tag]";
 	private static final String SUGGESTION_UNDO_UNDONE = "undo // undone <task number(s)>";
-	private static final String SUGGESTION_UNDO_UNDONE_UPDATE = "undo // undone <task number(s)> // update <task number>";
+	private static final String SUGGESTION_UNDO_UNDONE_UPDATE = "update <task number> // undo // undone <task number(s)>";
 	private static final String SUGGESTION_REDO_RESERVE = "redo // reserve <description> <start dates to end dates>";
 
 	private static final String COMMAND_ADD = "add ";
+
 	private static final String COMMAND_DELETE = "delete ";
+	private static final String COMMAND_DEL = "del ";
+
 	private static final String COMMAND_UPDATE = "update ";
 	private static final String COMMAND_EDIT = "edit ";
+
 	private static final String COMMAND_RESERVE = "reserve ";
+	private static final String COMMAND_RES = "res ";
+
 	private static final String COMMAND_CONFIRM = "confirm ";
+	private static final String COMMAND_CFM = "cfm ";
+
 	private static final String COMMAND_DONE = "done ";
 	private static final String COMMAND_UNDONE = "undone ";
+
+	private static final String COMMAND_SEARCH = "search";
 	private static final String COMMAND_SHOW = "show ";
+
 	private static final String COMMAND_POSTPONE = "postpone ";
+	private static final String COMMAND_PP = "pp ";
+
 	private static final String COMMAND_UNDO = "undo";
 	private static final String COMMAND_REDO = "redo";
+
 	private static final String COMMAND_FILEPATH = "change filepath";
+
 	private static final String COMMAND_HELP = "help";
 	private static final String COMMAND_EXIT = "exit";
 
 	private static InputSuggestion inputSuggester;
 	private static Display display = Logic.getDisplay();
+
 	private static boolean isFloatTask;
 	private static boolean isDeadlineTask;
 	private static boolean isEvent;
@@ -106,6 +139,8 @@ public class InputSuggestion {
 			}
 		}
 	}
+	
+	
 
 	public static InputSuggestion getInstance() {
 		if (inputSuggester == null) {
@@ -126,10 +161,16 @@ public class InputSuggestion {
 			return SUGGESTION_CONFIRM_FILEPATH;
 		} else if ("d".startsWith(currentInput)) {
 			return SUGGESTION_DELETE_DONE;
+		} else if ("del".startsWith(currentInput)) {
+			return SUGGESTION_DELETE_DEL;
 		} else if ("e".startsWith(currentInput)) {
 			return SUGGESTION_EDIT_EXIT;
+		} else if ("p".startsWith(currentInput)) {
+			return SUGGESTION_POSTPONE_PP;
 		} else if ("re".startsWith(currentInput)) {
 			return SUGGESTION_REDO_RESERVE;
+		} else if ("s".startsWith(currentInput)) {
+			return SUGGESTION_SHOW_SEARCH;
 		} else if ("u".startsWith(currentInput)) {
 			return SUGGESTION_UNDO_UNDONE_UPDATE;
 		} else if ("undo".startsWith(currentInput)) {
@@ -152,6 +193,8 @@ public class InputSuggestion {
 	private String getSuggestionAfterTypingCommand(String currentInput) {
 		if (currentInput.startsWith(COMMAND_ADD)) {
 			return SUGGESTION_ADD;
+		} else if (currentInput.startsWith(COMMAND_DEL)) {
+			return SUGGESTION_DEL;
 		} else if (currentInput.startsWith(COMMAND_DELETE)) {
 			return SUGGESTION_DELETE;
 		} else if (currentInput.startsWith(COMMAND_UPDATE)) {
@@ -186,16 +229,29 @@ public class InputSuggestion {
 			}
 		} else if (currentInput.startsWith(COMMAND_RESERVE)) {
 			return SUGGESTION_RESERVE;
+		} else if (currentInput.startsWith(COMMAND_RES)) {
+			return SUGGESTION_RES;
 		} else if (currentInput.startsWith(COMMAND_CONFIRM)) {
-			return SUGGESTION_CONFIRM;
+			determineTaskType(currentInput);
+			if (!isReservationTask) {
+				return SUGGESTION_INVALID_TASK;
+			} else {
+				return SUGGESTION_CONFIRM;
+			}
+		} else if (currentInput.startsWith(COMMAND_CFM)) {
+			return SUGGESTION_CFM;
 		} else if (currentInput.startsWith(COMMAND_DONE)) {
 			return SUGGESTION_DONE;
 		} else if (currentInput.startsWith(COMMAND_UNDONE)) {
 			return SUGGESTION_UNDONE;
 		} else if (currentInput.startsWith(COMMAND_SHOW)) {
 			return SUGGESTION_SHOW;
+		} else if (currentInput.startsWith(COMMAND_SEARCH)) {
+			return SUGGESTION_SEARCH;
 		} else if (currentInput.startsWith(COMMAND_POSTPONE)) {
 			return SUGGESTION_POSTPONE;
+		} else if (currentInput.startsWith(COMMAND_PP)) {
+			return SUGGESTION_PP;
 		} else if (currentInput.equals(COMMAND_UNDO)) {
 			return SUGGESTION_UNDO;
 		} else if (currentInput.equals(COMMAND_REDO)) {
@@ -216,17 +272,21 @@ public class InputSuggestion {
 
 		suggestions.add(SUGGESTION_ADD);
 		suggestions.add(SUGGESTION_RESERVE);
+		suggestions.add(SUGGESTION_DEL);
 		suggestions.add(SUGGESTION_DELETE);
 		suggestions.add(SUGGESTION_UPDATE);
 		suggestions.add(SUGGESTION_EDIT);
 		suggestions.add(SUGGESTION_SHOW);
+		suggestions.add(SUGGESTION_SEARCH);
 		suggestions.add(SUGGESTION_CONFIRM);
+		suggestions.add(SUGGESTION_CFM);
 		suggestions.add(SUGGESTION_DONE);
 		suggestions.add(SUGGESTION_UNDONE);
 		suggestions.add(SUGGESTION_UNDO);
 		suggestions.add(SUGGESTION_REDO);
 		suggestions.add(SUGGESTION_FILEPATH);
 		suggestions.add(SUGGESTION_POSTPONE);
+		suggestions.add(SUGGESTION_PP);
 		suggestions.add(SUGGESTION_HELP);
 		suggestions.add(SUGGESTION_EXIT);
 
@@ -234,3 +294,4 @@ public class InputSuggestion {
 	}
 
 }
+
